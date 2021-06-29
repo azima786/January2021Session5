cf1d885ae75719e1cd27af9ea7ff71adafc09da1
azima786 <azima786@gmail.com>
2021-06-29 13:00:15 -0500

    second origin

---
+++ src/main/java/VariousConcepts/gittest.java
@@ -0,0 +1,12 @@
+package VariousConcepts;
+
+public class gittest {
+
+	public static void main(String[] args) {
+		printme();
+	}
+	
+	public static void printme() {
+		System.out.println("Git Test");
+	}
+}
